906fe16e23351cb50ad43d600d325139d8cb694a
Espen Flo Bødal <espen.bodal@sintef.no>
2022-03-02 18:42:25 +0100

    Cleaning up model repository

--- demo/run_detInvModel.py
+++ demo/run_detInvModel.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Dec 20 13:23:15 2018
+# %% [markdown]
+# Created on Thu Dec 20 13:23:15 2018
+#
+# @author: espenfb
 
-@author: espenfb
-"""
+# %%
+import os
+import sys
+from pathlib import Path
+
+filepath = os.path.abspath('')
+sys.path.append(str(Path(filepath) / "src"))
 
 import detInvModel as dim
 import pandas as pd
@@ -11,20 +18,24 @@ import savedRes as sr
 
 time_data = {
 'start_date': pd.Timestamp(year = 2015, month = 1, day = 1),
-'end_date': pd.Timestamp(year = 2015, month = 1, day = 5),
+'end_date': pd.Timestamp(year = 2016, month = 1, day = 1),
 'ref_date': pd.Timestamp(year = 2015, month = 1, day = 1)}
 dirs = {
-'data_dir' : "Data\\",
+'data_dir' : "demo\\data\\",
 'ctrl_data_file' : 'ctrl_data.csv',
-'res_dir' : 'Result_test_master\\'}
+'res_dir' : 'results\\'}
+
+
+# %%
 obj = dim.deterministicModel(time_data, dirs)
 
+# %%
 obj.buildModel()
 
+# %%
 obj.solve()
-#
-#obj.printModel()
-#
+
+# %%
 obj.processResults()
 
 obj.saveRes(dirs['res_dir'])
